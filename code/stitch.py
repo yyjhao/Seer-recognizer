a0c55a6f019754d45bd020e005026cbdd950b7b9
Yujian <yyjhao@gmail.com>
2015-10-30 12:39:01 +0800

    Update stitch to awesome

--- code/stitch.py
+++ code/stitch.py
@@ -3,23 +3,24 @@ import cv2.cv as cv
 import numpy as np
 import stitcher
 import matplotlib.pyplot as plt
+import sys
 
-right_add = 3500
-left_add = 3200
+right_add = 3600
+left_add = 2700
+top_cutoff = 50
 
 lsrc = np.zeros((4, 2), dtype="float32")
 ldst = np.zeros((4, 2), dtype="float32")
 
 lsrc[0] = [447, 660]
 lsrc[1] = [1763, 314]
-lsrc[2] = [1919, 333]
+lsrc[2] = [1920, 333]
 lsrc[3] = [1539, 1079]
 
-
-ldst[0] = [3230 - 5000 + left_add, 1028]
-ldst[1] = [5245 - 5000 + left_add, 243]
-ldst[2] = [5408 - 5000 + left_add, 242]
-ldst[3] = [5231 - 5000 + left_add, 1010]
+ldst[0] = [3230 - 5000 + left_add + 230, 1028 - 30]
+ldst[1] = [5245 - 5000 + left_add - 7, 243]
+ldst[2] = [5408 - 5000 + left_add - 28, 242]
+ldst[3] = [5231 - 5000 + left_add - 40, 1010]
 
 lptran = cv2.getPerspectiveTransform(lsrc, ldst)
 
@@ -28,27 +29,76 @@ lsrc[1] = [0, 1056]
 lsrc[2] = [475, 924]
 lsrc[3] = [407, 220]
 
-
-ldst[0] = [6507 - 5000 + left_add, 234]
+ldst[0] = [6507 - 5000 + left_add + 15, 234]
 ldst[1] = [6435 - 5000 + left_add, 1003]
-ldst[2] = [6920 - 5000 + left_add, 1002]
-ldst[3] = [6920 - 5000 + left_add, 230]
+ldst[2] = [6920 - 5000 + left_add + 5, 1002]
+ldst[3] = [6920 - 5000 + left_add + 25, 230]
 
 rptran = cv2.getPerspectiveTransform(lsrc, ldst)
 
 atran = np.float32([[1, 0, left_add], [0, 1, 0]])
+toptran = np.float32([[1, 0, 0], [0, 1, -top_cutoff]])
+leftonetran = np.float32([[1, 0, -3], [0, 1, -1]])
 
-def overlap(top_image, bottom_image):
-    ret, tgray = cv2.threshold(cv2.cvtColor(top_image, cv2.COLOR_BGR2GRAY), 30, 255, cv2.THRESH_BINARY)
-    ret, mtgray = cv2.threshold(cv2.cvtColor(bottom_image, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY)
-    intersect = cv2.bitwise_and(tgray, mtgray)
-    return bottom_image - cv2.bitwise_and(bottom_image, bottom_image, mask=intersect) + top_image
+def remove_left(img, left):
+    tran = np.float32([[1, 0, -left], [0, 1, 0]])
+    img = cv2.warpAffine(img, tran, img.shape)
+    tran = np.float32([[1, 0, left], [0, 1, 0]])
+    img = cv2.warpAffine(img, tran, img.shape)
+    return img
+
+
+left_cached = False
+right_cached = False
+
+left_intersect = None
+right_intersect = None
+left_intersect_line = None
+right_intersect_line = None
+
+def overlap(top_image, bottom_image, left):
+    global left_cached
+    global right_cached
+    global left_intersect_line
+    global right_intersect_line
+    global left_intersect
+    global right_intersect
+    if left:
+        if not left_cached:
+            ret, tgray = cv2.threshold(cv2.cvtColor(top_image, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY)
+            ret, mtgray = cv2.threshold(cv2.cvtColor(bottom_image, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY)
+            left_intersect = cv2.bitwise_and(tgray, mtgray)
+            shiftintersect = cv2.warpAffine(left_intersect, leftonetran, (left_intersect.shape[1], left_intersect.shape[0]))
+            left_intersect_line = left_intersect - shiftintersect
+            left_intersect_line = remove_left(left_intersect_line, left_add + 3)
+            left_cached = True
+        intersect = left_intersect
+        intersect_line = left_intersect_line
+
+        mtinter = cv2.bitwise_and(bottom_image, bottom_image, mask=intersect)
+        linemtinter = cv2.bitwise_and(bottom_image, bottom_image, mask=intersect_line)
+        linetinter = cv2.bitwise_and(top_image, top_image, mask=intersect_line)
+
+        return bottom_image - mtinter + top_image + linemtinter - linetinter
+    else:
+        if not right_cached:
+            ret, tgray = cv2.threshold(cv2.cvtColor(top_image, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY)
+            ret, mtgray = cv2.threshold(cv2.cvtColor(bottom_image, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY)
+            right_intersect = cv2.bitwise_and(tgray, mtgray)
+            right_cached = True
+        intersect = right_intersect
+
+        mtinter = cv2.bitwise_and(bottom_image, bottom_image, mask=intersect)
+
+        return bottom_image - mtinter + top_image
+
 
 def stitch_pics(l, m, r):
     mt = cv2.warpAffine(m, atran, (m.shape[1] + left_add + right_add, m.shape[0]))
     lt = cv2.warpPerspective(l, lptran, (mt.shape[1], mt.shape[0]))
     rt = cv2.warpPerspective(r, rptran, (mt.shape[1], mt.shape[0]))
-    return overlap(overlap(lt, mt), rt)
+    return overlap(overlap(lt, mt, True), rt, False)
+
 
 mid = cv2.VideoCapture("./football_mid.mp4")
 left = cv2.VideoCapture("./football_left.mp4")
@@ -60,10 +110,12 @@ right_w = right.get(cv.CV_CAP_PROP_FRAME_WIDTH)
 
 h = left.get(cv.CV_CAP_PROP_FRAME_HEIGHT)
 
-fps = left_w.get(cv2.cv.CV_CAP_PROP_FPS)
+fps = left.get(cv2.cv.CV_CAP_PROP_FPS)
+
+movie_shape = (int(left_add + mid_w + right_add), int(h - top_cutoff))
 
 fourcc = cv2.cv.CV_FOURCC(*"MPEG")
-output = cv2.VideoWriter('./stitched.mpeg', fourcc, fps, (int(left_add + mid_w + right_add), int(h)))
+output = cv2.VideoWriter('./stitched.mpeg', fourcc, fps, movie_shape)
 
 count = 0
 while True:
@@ -74,6 +126,6 @@ while True:
     retr, r = right.read()
     if not (retl and retm and retr):
         break
-    output.write(stitch_pics(l, m, r))
+    output.write(cv2.warpAffine(stitch_pics(l, m, r), toptran, movie_shape))
 
 output.release()
