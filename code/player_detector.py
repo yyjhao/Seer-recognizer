7c44e185e30b86eefda20355ff0a2c6d06c2049b
Larry Xu <larry@larryxu.com>
2015-11-06 18:45:49 +0800

    Cleanup player_detector.py

--- code/player_detector.py
+++ code/player_detector.py
@@ -3,7 +3,6 @@ import cv2
 
 # Read background image
 BACKGROUND_IMG = cv2.imread('../images/stitched_background.png')
-# BACKGROUND_IMG = cv2.imread('../images/background2.png')
 BACKGROUND_IMG_HSV = np.float32(cv2.cvtColor(BACKGROUND_IMG, cv2.COLOR_BGR2HSV))
 BACKGROUND_IMG_LAB = np.float32(cv2.cvtColor(BACKGROUND_IMG, cv2.COLOR_BGR2LAB))
 
@@ -20,7 +19,7 @@ VALUE_HIGH_THRESH = 0.95
 SATURATION_THRESH = 60
 HUE_THRESH = 4
 
-# Several linear scaled threshold values based on row
+# Variable thresholding based on row position
 def threshVal(row):
     if row < 175:
         return 100
@@ -37,7 +36,7 @@ def threshVal(row):
     # 0-1029 -> 8-51
     return float(row) / 1029 * 43 + 8
 
-# Several linear scaled area filters based on row
+# Variable contour area filtering based on row position
 def areaVal(row):
     if row < 260:
         # 0-1029 -> 0-1200
@@ -48,12 +47,11 @@ def areaVal(row):
     elif row < 740:
         # 0-1029 -> 0-2200
         return float(row) / 1029 * 2200
-    # 0-1029 -> 0-4300
+    # 0-1029 -> 0-3800
     return float(row) / 1029 * 3800
 
-# Generates a mask where the area inside the polygon
-# specified by the 4 pts is set to 1, and everything
-# else is set to 0
+# Generates a mask where the area inside the polygon specified
+# by the pts is set to 1, and everything else is set to 0
 def generateMask(pts):
     mask = np.zeros(BACKGROUND_IMG[:,:,0].shape)
     cv2.fillConvexPoly(mask, pts, 1)
@@ -70,7 +68,7 @@ FIELD_PTS = np.array(
     [-100, 990]])
 FIELD_MASK = generateMask(FIELD_PTS)
 
-# CUZ THE BLUE GOALIE IS A NINJA
+# Area of the field blue goalie is standing in
 BLUE_GOALIE_PTS = np.array(
     [[5240, 267],
     [5940, 267],
@@ -78,7 +76,7 @@ BLUE_GOALIE_PTS = np.array(
     [5240, 400]])
 BLUE_GOALIE_MASK = generateMask(BLUE_GOALIE_PTS).astype('bool')
 
-# The field has darker shadows in this part of the field
+# Area of the field which has shadows that need to be removed
 SHADOW_PTS = np.array(
     [[3200, 190],
     [5030, 178],
@@ -92,6 +90,7 @@ SHADOW_MASK = generateMask(SHADOW_PTS).astype('bool')
 #   1. Are too small
 #   2. Have a wide aspect ratio
 #   3. Are outside the field boundary
+# Note: Special area filter in the blue goalie area is used
 def contourFilter(contour_bound):
     x, y, w, h = contour_bound[1]
     check = w * h > areaVal(y + h) \
@@ -101,8 +100,9 @@ def contourFilter(contour_bound):
         check = check and w * h > 1050
     return check
 
-# Creates a mask where all shadows identified in the given frame
-# are given the value True, and all other points are False
+# Creates a mask where all shadows identified in the given frame are given the
+# value True, and all other points are False. Uses a chromacity-based method
+# of comparing HSV values of the frame to the background to determine shadows.
 def shadowMask(frame_hsv):
     frame_hsv_float = np.float32(frame_hsv)
     hue_diff = np.absolute(frame_hsv_float[:,:,0] - BACKGROUND_IMG_HSV[:,:,0])
@@ -134,19 +134,21 @@ def getPlayers(frame):
     # Normalize the Delta E difference to a value between 0-255
     delta_E = delta_E / delta_E.max() * 255
 
-    # Threshold the Delta E difference so all values greater than the thresh_val
-    # are set to 255 (foreground), and all values lower are set to 0 (background).
-    # This creates a binary image used for contour detection.
-    # Use variable thresholding based on the row coordinate of the image.
+    # Threshold the Delta E difference so all foreground pixels are set to 255,
+    # and all background pixels are set to 0. This creates a binary image used
+    # for contour detection. Since the illumination is different across the
+    # field, use variable thresholding based on the row coordinate of the image.
+    # Note: Blue goalie area receives a lower threshold since he is very hard
+    # to detect
     thresh_vals = np.zeros(delta_E.shape)
     for row in range(thresh_vals.shape[0]):
         thresh_vals[row,:] = threshVal(row)
     thresh_vals[BLUE_GOALIE_MASK] = 10
     binary_frame = np.zeros(delta_E.shape, 'uint8')
     binary_frame[delta_E > thresh_vals] = 255
-    # cv2.imwrite('../images/player_detection/binaries/{}.png'.format(k), binary_frame)
+
+    # Apply shadow removal to the binary image
     binary_frame[shadowMask(frame_hsv)] = 0
-    # cv2.imwrite('../images/player_detection/shadow_removed/{}.png'.format(k), binary_frame)
 
     # Find all outer contours in the binary image
     contours, h = cv2.findContours(binary_frame, cv2.RETR_EXTERNAL,
@@ -161,17 +163,24 @@ def getPlayers(frame):
     for cb in contour_bounds:
         contour = cb[0]
         bounding_rect = cb[1]
+
+        # Create a mask of the contour interior. First shift all the contour
+        # points and create a small mask the size of it's bounding rectangle.
+        # Then grab the nonzero indices corresponding to the contour interior
+        # and shift them back to their appropriate positions in the frame.
+        # This is to more efficiently calculate the contour interior indices
+        # without having to generate a mask the size of the frame.
         for pt in contour:
             pt[0][0] = pt[0][0] - bounding_rect[0]
             pt[0][1] = pt[0][1] - bounding_rect[1]
-
-        # Create a mask where the contour interiors are set to 255,
-        # and all other values are 0. Use the mask to grab all hue
-        # and values within the contour.
-        mask = np.zeros([bounding_rect[3],bounding_rect[2]]).astype('uint8')
+        mask = np.zeros([bounding_rect[3], bounding_rect[2]]).astype('uint8')
         cv2.drawContours(mask, [contour], 0, 255, -1)
         indices = np.nonzero(mask)
-        shifted_indices = (indices[0]+bounding_rect[1],indices[1]+bounding_rect[0])
+        shifted_indices = (indices[0] + bounding_rect[1],
+                           indices[1] + bounding_rect[0])
+
+        # Use the mask to grab all hue and values within the contour. Shift the
+        # hue values by 10*2 degrees to better separate red and blue hues.
         hues = frame_hue[shifted_indices]
         shifted_hues = np.remainder(hues + 10, 180)
         values = frame_value[shifted_indices]
@@ -198,9 +207,10 @@ def getPlayers(frame):
             # goalies and referee
             color = Color.GREEN
 
-        # Append the player bounding rectangle and color to the list
+        # Append the player bounding rectangle and color to the list.
+        # Try to filter out linesmen at the bottom.
         if color != Color.GREEN or bounding_rect[1] < 800:
-            player = [bounding_rect, color, mean_hue, mean_value]
+            player = [bounding_rect, color]
             players.append(player)
 
     # Rightmost green player is blue goalie and
@@ -211,99 +221,62 @@ def getPlayers(frame):
     sorted_green_players[0][1] = Color.RED_GOALIE
     sorted_green_players[-1][1] = Color.BLUE_GOALIE
 
-    # contours = [cb[0] for cb in contour_bounds]
-    # contour_frame = np.zeros(frame.shape)
-    # contour_frame[:,:,0] = delta_E
-    # contour_frame[:,:,1] = delta_E
-    # contour_frame[:,:,2] = delta_E
-    # cv2.drawContours(contour_frame, contours, -1, (0, 255, 0), 1)
-    # cv2.imwrite('../images/player_detection/contours/{}.png'.format(k), contour_frame)
+    players = [(pl[0], pl[1]) for pl in players]
 
     return players
 
 # Runs player detection on each frame of the stitched football video.
-# Each player detected frame is then saved upon stepping through the frames.
-# NOTE: Frames are read from a folder of PNG files instead of the MPEG
+# Each player-detected frame is then saved as a PNG image.
+# Note: Frames are read from a folder of PNG files instead of the MPEG
 # video in order to combat lossy video compression
-def main():
+def generateDetectionFrames():
+    frame_offset = 0
     frame_count = 7200
 
-
-    # Read each frame
-    for k in range(0, frame_count, 5):
+    for k in range(frame_offset, frame_count):
+        # Read each frame
         frame = cv2.imread('../images/stitched_frames/{}.png'.format(k))
 
         # Detect players in frame
-        players = getPlayers(frame, k)
-        detection_frame = frame.copy()
+        players = getPlayers(frame)
+
         for player in players:
             x, y, w, h = player[0]
             color = player[1]
-            # Draw the bounding rectangle around each detected player
-            cv2.rectangle(detection_frame, (x,y), (x+w,y+h), color, 1)
-            text = "{}, {}, {}, {:.2f}, {}, {}".format(x, y, w * h, float(w) / h, int(player[2]), int(player[3]))
-            cv2.putText(detection_frame, text, (x,y), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (255, 255, 255))
 
-        # Show and save the player detected frame
-        cv2.imwrite('../images/player_detection/detections/{}.png'.format(k), detection_frame)
+            # Draw the bounding rectangle and its coordinates around each player
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 1)
+            text = "{}, {}, {}, {}".format(x, y, w, h)
+            cv2.putText(frame, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.35, (255, 255, 255))
+
+        # Save the player-detected frame
+        cv2.imwrite('../images/player_detection/detections/{}.png'.format(k), frame)
+
         print "frame", k
 
-def main2():
+# Runs player detection on each frame of the stitched football video.
+# Each players list is then saved in the output file as a separate line.
+# Note: Frames are read from a folder of PNG files instead of the MPEG
+# video in order to combat lossy video compression
+def generatePlayersList():
+    output_file = 'players.txt'
+    frame_offset = 0
     frame_count = 7200
-    start_frame = 0
-    end_frame = 650
 
-    # Read each frame
-    with open("players.txt", 'w') as fout:
-        for k in range(start_frame, end_frame):
+    with open(output_file, 'w') as fout:
+        for k in range(frame_offset, frame_count):
+            # Read each frame
             frame = cv2.imread('../images/stitched_frames/{}.png'.format(k))
-            # frame = cv2.imread('../images/test.png')
 
             # Detect players in frame
             players = getPlayers(frame)
+
+            # Write players list to output file
             fout.write(str(players))
-            fout.write("\n")
+            fout.write('\n')
 
-            # detection_frame = frame.copy()
-            # for player in players:
-            #   x, y, w, h = player[0]
-            #   color = player[1]
-            #   # Draw the bounding rectangle around each detected player
-            #   cv2.rectangle(detection_frame, (x,y), (x+w,y+h), color, 1)
-
-            # # Show and save the player detected frame
-            # cv2.imwrite('../images/player_detection/detections/{}.png'.format(k), detection_frame)
-            # quit()
-
-            # Show and save the player detected frame
             print "frame", k
-
-def main3():
-    frame_count = 7200
-    offset = 0
-
-    with open("players_test7.txt", 'w') as fout:
-        # Read each frame
-        for k in range(frame_count-offset):
-            frame = cv2.imread('../images/stitched_frames/{}.png'.format(k+offset))
-
-            # Detect players in frame
-            players = getPlayers(frame)
-            # detection_frame = frame.copy()
-            # for player in players:
-            #     x, y, w, h = player[0]
-            #     color = player[1]
-            #     # Draw the bounding rectangle around each detected player
-            #     cv2.rectangle(detection_frame, (x,y), (x+w,y+h), color, 1)
-            #     text = "{}, {}, {}, {:.2f}, {}, {}".format(x, y, w * h, float(w) / h, int(player[2]), int(player[3]))
-            #     cv2.putText(detection_frame, text, (x,y), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (255, 255, 255))
-
-            # Show and save the player detected frame
-            # cv2.imwrite('../images/player_detection/detections/{}.png'.format(k), detection_frame)
-            p = [(pl[0], pl[1]) for pl in players]
-            fout.write(str(p))
-            fout.write("\n")
-            print "frame", k+offset
 
 def generateVideoWithRect(inputVideoPath, inputPlayerPath, outputVideoPath):
     with open(inputPlayerPath) as fin:
@@ -333,4 +306,4 @@ def generateVideoWithRect(inputVideoPath, inputPlayerPath, outputVideoPath):
     cap.release()
     
 if __name__ == "__main__":
-    main3()
+    generateDetectionFrames()
