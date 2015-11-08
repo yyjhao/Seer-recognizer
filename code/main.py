f0b9a7180a6f00b7ba0c228f45a02c75821997f9
Dennis Schmidt <schmiddi11@gmx.de>
2015-11-08 13:01:33 +0800

    Create a stitch function + adjust main
    - Create a stitch function, such that it is callable
    - Adjust the function calls in the main.py file

--- code/main.py
+++ code/main.py
@@ -2,21 +2,28 @@ import player_displayer
 import player_detector
 from videoComposition import createFinalVideo
 from util import deleteFirstNLines
+from stitch import stitch
 
+'''
+    The main function call all the functions necessary to get from 
+    the 3 single videos to the final composition with the detected
+    players. If one is only interested in running a part, just 
+    comment the other parts out.
+'''
 if __name__ == '__main__':
     # Stitch the video
-    # run stitch.py
+    stitch()
     # Detect the players
-    # player_detector.main3()
-    # player_detector.generateVideoWithRect("../videos/stitched.mpeg", "players_cat.txt","../videos/stitched_rect.mpeg")
+    player_detector.generatePlayersList()
+    player_detector.generateVideoWithRect("../videos/stitched.mpeg", "players_cat.txt","../videos/stitched_rect.mpeg")
     # Remove the first 4 lines of the input file
-    # deleteFirstNLines("players_cat.txt", "players_cat_wo4.txt", 4)
+    deleteFirstNLines("players_cat.txt", "players_cat_wo4.txt", 4)
     # Generate the top down data
-    # player_displayer.playerDataToSmoothedTopDown("players_cat_wo4.txt")
+    player_displayer.playerDataToSmoothedTopDown("players_cat_wo4.txt")
     # Generate top down video
-    # player_displayer.createTopDownVideo()
+    player_displayer.createTopDownVideo()
     # Generate heatmaps
-    # _, playersPos = player_displayer.getPlayerWiseTopDown()
-    # player_displayer.generateHeatmaps(playersPos)
+    _, playersPos = player_displayer.getPlayerWiseTopDown()
+    player_displayer.generateHeatmaps(playersPos)
     # Generate final Video
     createFinalVideo()
--- code/stitch.py
+++ code/stitch.py
@@ -2,46 +2,6 @@ import cv2
 import cv2.cv as cv
 import numpy as np
 
-right_add = 3600
-left_add = 2700
-top_cutoff = 50
-
-lsrc = np.zeros((4, 2), dtype="float32")
-ldst = np.zeros((4, 2), dtype="float32")
-
-# hand ajusted quadrangle vertices to compute the perspective transform matrix
-lsrc[0] = [447, 660]
-lsrc[1] = [1763, 314]
-lsrc[2] = [1920, 333]
-lsrc[3] = [1539, 1079]
-
-ldst[0] = [3230 - 5000 + left_add + 230, 1028 - 30]
-ldst[1] = [5245 - 5000 + left_add - 7, 243]
-ldst[2] = [5408 - 5000 + left_add - 28, 242]
-ldst[3] = [5231 - 5000 + left_add - 40, 1010]
-
-lptran = cv2.getPerspectiveTransform(lsrc, ldst)
-
-lsrc[0] = [0, 226]
-lsrc[1] = [0, 1056]
-lsrc[2] = [475, 924]
-lsrc[3] = [407, 220]
-
-ldst[0] = [6507 - 5000 + left_add + 15, 234]
-ldst[1] = [6435 - 5000 + left_add, 1003]
-ldst[2] = [6920 - 5000 + left_add + 5, 1002]
-ldst[3] = [6920 - 5000 + left_add + 25, 230]
-
-rptran = cv2.getPerspectiveTransform(lsrc, ldst)
-
-# affine transform matrix to move the middle frame to the right
-atran = np.float32([[1, 0, left_add], [0, 1, 0]])
-# affine transform matrix to cut of some part of the top
-toptran = np.float32([[1, 0, 0], [0, 1, -top_cutoff]])
-# affine transform matrix to move the mask a bit to remove black lines
-leftonetran = np.float32([[1, 0, -3], [0, 1, -1]])
-
-
 # remove {left} pixels from the left of the image
 def remove_left(img, left):
     tran = np.float32([[1, 0, -left], [0, 1, 0]])
@@ -49,16 +9,6 @@ def remove_left(img, left):
     tran = np.float32([[1, 0, left], [0, 1, 0]])
     img = cv2.warpAffine(img, tran, img.shape)
     return img
-
-
-# cache for mask images
-left_cached = False
-right_cached = False
-
-left_intersect = None
-right_intersect = None
-left_intersect_line = None
-right_intersect_line = None
 
 
 # This function overlaps 2 images together
@@ -72,7 +22,7 @@ right_intersect_line = None
 # then remove the masked region
 # However, transformed image has a black stripe at its left and right so we need to
 # remove that as well
-def overlap(top_image, bottom_image, left):
+def overlap(top_image, bottom_image, left, leftonetran, left_add):
     global left_cached
     global right_cached
     global left_intersect_line
@@ -117,41 +67,93 @@ def overlap(top_image, bottom_image, left):
         return bottom_image - mtinter + top_image
 
 
-def stitch_pics(l, m, r):
+def stitch_pics(l, m, r, atran, lptran, rptran, leftonetran, right_add, left_add):
     # first move the images appropriately, then overlap them
     mt = cv2.warpAffine(m, atran, (m.shape[1] + left_add + right_add, m.shape[0]))
     lt = cv2.warpPerspective(l, lptran, (mt.shape[1], mt.shape[0]))
     rt = cv2.warpPerspective(r, rptran, (mt.shape[1], mt.shape[0]))
-    return overlap(overlap(lt, mt, True), rt, False)
+    return overlap(overlap(lt, mt, True), rt, False, leftonetran, left_add)
 
 
-mid = cv2.VideoCapture("./football_mid.mp4")
-left = cv2.VideoCapture("./football_left.mp4")
-right = cv2.VideoCapture("./football_right.mp4")
+def stitch():
+    right_add = 3600
+    left_add = 2700
+    top_cutoff = 50
 
-left_w = left.get(cv.CV_CAP_PROP_FRAME_WIDTH)
-mid_w = mid.get(cv.CV_CAP_PROP_FRAME_WIDTH)
-right_w = right.get(cv.CV_CAP_PROP_FRAME_WIDTH)
+    lsrc = np.zeros((4, 2), dtype="float32")
+    ldst = np.zeros((4, 2), dtype="float32")
+    
+    # hand ajusted quadrangle vertices to compute the perspective transform matrix
+    lsrc[0] = [447, 660]
+    lsrc[1] = [1763, 314]
+    lsrc[2] = [1920, 333]
+    lsrc[3] = [1539, 1079]
 
-h = left.get(cv.CV_CAP_PROP_FRAME_HEIGHT)
+    ldst[0] = [3230 - 5000 + left_add + 230, 1028 - 30]
+    ldst[1] = [5245 - 5000 + left_add - 7, 243]
+    ldst[2] = [5408 - 5000 + left_add - 28, 242]
+    ldst[3] = [5231 - 5000 + left_add - 40, 1010]
+    
+    lptran = cv2.getPerspectiveTransform(lsrc, ldst)
+    
+    lsrc[0] = [0, 226]
+    lsrc[1] = [0, 1056]
+    lsrc[2] = [475, 924]
+    lsrc[3] = [407, 220]
 
-fps = left.get(cv2.cv.CV_CAP_PROP_FPS)
+    ldst[0] = [6507 - 5000 + left_add + 15, 234]
+    ldst[1] = [6435 - 5000 + left_add, 1003]
+    ldst[2] = [6920 - 5000 + left_add + 5, 1002]
+    ldst[3] = [6920 - 5000 + left_add + 25, 230]
+    
+    rptran = cv2.getPerspectiveTransform(lsrc, ldst)
+    
+    # affine transform matrix to move the middle frame to the right
+    atran = np.float32([[1, 0, left_add], [0, 1, 0]])
+    # affine transform matrix to cut of some part of the top
+    toptran = np.float32([[1, 0, 0], [0, 1, -top_cutoff]])
+    # affine transform matrix to move the mask a bit to remove black lines
+    leftonetran = np.float32([[1, 0, -3], [0, 1, -1]])
 
-movie_shape = (int(left_add + mid_w + right_add), int(h - top_cutoff))
+    # cache for mask images
+    left_cached = False
+    right_cached = False
+    
+    left_intersect = None
+    right_intersect = None
+    left_intersect_line = None
+    #right_intersect_line = None
 
-# have to use MPEG cuz huge dimension
-fourcc = cv2.cv.CV_FOURCC(*"MPEG")
-output = cv2.VideoWriter('./stitched.mpeg', fourcc, fps, movie_shape)
+    mid = cv2.VideoCapture("./football_mid.mp4")
+    left = cv2.VideoCapture("./football_left.mp4")
+    right = cv2.VideoCapture("./football_right.mp4")
 
-count = 0
-while True:
-    count += 1
-    print "frame", count
-    retl, l = left.read()
-    retm, m = mid.read()
-    retr, r = right.read()
-    if not (retl and retm and retr):
-        break
-    output.write(cv2.warpAffine(stitch_pics(l, m, r), toptran, movie_shape))
+    #left_w = left.get(cv.CV_CAP_PROP_FRAME_WIDTH)
+    mid_w = mid.get(cv.CV_CAP_PROP_FRAME_WIDTH)
+    #right_w = right.get(cv.CV_CAP_PROP_FRAME_WIDTH)
+    
+    h = left.get(cv.CV_CAP_PROP_FRAME_HEIGHT)
+    
+    fps = left.get(cv2.cv.CV_CAP_PROP_FPS)
+    
+    movie_shape = (int(left_add + mid_w + right_add), int(h - top_cutoff))
+    
+    # have to use MPEG cuz huge dimension
+    fourcc = cv2.cv.CV_FOURCC(*"MPEG")
+    output = cv2.VideoWriter('./stitched.mpeg', fourcc, fps, movie_shape)
+    
+    count = 0
+    while True:
+        count += 1
+        print "frame", count
+        retl, l = left.read()
+        retm, m = mid.read()
+        retr, r = right.read()
+        if not (retl and retm and retr):
+            break
+        output.write(cv2.warpAffine(stitch_pics(l, m, r, atran, lptran, rptran, leftonetran, right_add, left_add), toptran, movie_shape))
+    
+    output.release()
 
-output.release()
+if __name__ == '__main__':
+    stitch()
