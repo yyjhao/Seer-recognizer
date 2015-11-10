caeb23cb8c3f29b8e2a421de3a28b9297f68be78
Yujian <yyjhao@gmail.com>
2015-11-10 17:27:03 +0800

    background canz fromz vid

--- code/gen_background.py
+++ code/gen_background.py
@@ -1,10 +1,13 @@
 import cv2
 import numpy as np
 
-background = cv2.imread("../images/stitched_frames/0.png")
-background = np.zeros(background.shape)
+# background = cv2.imread("../images/stitched_frames/0.png")
+cap = cv2.VideoCapture('../videos/stitched.mpeg')
+movie_shape = (int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)), int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)))
+background = np.zeros(movie_shape)
 for i in range(0, 7200):
-    frame = cv2.imread("../images/stitched_frames/{0}.png".format(i))
+    # frame = cv2.imread("../images/stitched_frames/{0}.png".format(i))
+    _, frame = cap.read()
     background += frame
     print i
 
